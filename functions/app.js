579ded4f2096950c00430cec2d6cbed3ec55352c
Md Kashif Zafar <110238257+kashi159@users.noreply.github.com>
2024-03-20 14:39:07 +0530

    made netlify deployment ready

--- functions/app.js
+++ functions/app.js
@@ -1,13 +1,13 @@
 import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
 
-import typeDefsBook from './schemas/Book.js'
-import typeDefsUser from './schemas/User.js'
-import booksResolvers from './resolvers/books.js'      
-import usersResolvers from './resolvers/users.js'
+import typeDefsBook from '../schemas/Book.js'
+import typeDefsUser from '../schemas/User.js'
+import booksResolvers from '../resolvers/books.js'      
+import usersResolvers from '../resolvers/users.js'
 
-import authenticate  from './middleware/authenticate.js'
-import sequelize from './utils/database.js'
+import authenticate  from '../middleware/authenticate.js'
+import sequelize from '../utils/database.js'
 
 
 const server = new ApolloServer({
