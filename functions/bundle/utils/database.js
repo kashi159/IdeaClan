a1c7e936ca73fe658ce55820326a6cdde8a900c3
Md Kashif Zafar <110238257+kashi159@users.noreply.github.com>
2024-03-21 02:00:49 +0530

    added models

---
+++ functions/bundle/utils/database.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize');
+// import Sequelize from 'sequelize';
+require('dotenv').config();
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+const sequelize = new Sequelize(process.env.DB_NAME , process.env.DB_USER , process.env.DB_PASSWORD , {
+    dialect: 'mysql',
+    host: process.env.DB_HOST_NAME,
+    port: process.env.DB_PORT,
+    timezone: '+05:30', // Set the timezone offset for the database connection
+})
+// export default sequelize;
+module.exports = sequelize;
