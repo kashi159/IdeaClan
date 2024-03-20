ca313970fe3fb3ddb63896717e03f9ae5637ac35
Md Kashif Zafar <110238257+kashi159@users.noreply.github.com>
2024-03-21 01:26:45 +0530

    bundled

---
+++ functions/bundle/server.js
@@ -0,0 +1,46 @@
+const ApolloServer = require('apollo-server').ApolloServer
+const ApolloServerLambda = require('apollo-server-lambda').ApolloServer
+const { gql } = require('apollo-server-lambda');
+
+const typeDefsBook = require('../schemas/Book');
+const typeDefsUser = require('../schemas/User');
+const booksResolvers = require('../resolvers/books');
+const usersResolvers = require('../resolvers/users');
+const authenticate = require('../middleware/authenticate');
+const sequelize = require('../utils/database');
+
+function createLambdaServer() {
+    return new ApolloServerLambda({
+        typeDefs: [typeDefsBook, typeDefsUser],
+        resolvers: [booksResolvers, usersResolvers],
+        mocks: true,
+        playground: true,
+        context: async ({ event, context }) => {
+            const user = await authenticate(event)
+            return user
+        }
+    });
+}
+
+function createLocalServer() {
+    return new ApolloServer({
+        typeDefs: [typeDefsBook, typeDefsUser],
+        resolvers: [booksResolvers, usersResolvers],
+        mocks: true,
+        playground: true,
+        context: async ({ event, context }) => {
+            const user = await authenticate(event)
+            return user
+        }
+    });
+}
+
+sequelize.sync({ force: process.env.FORCE_DB_SYNC === 'true' })
+    .then(() => {
+        console.log('Database synchronization successful')
+    })
+    .catch((error) => {
+        console.error('Error synchronizing database:', error)
+    });
+
+module.exports = { createLambdaServer, createLocalServer };
